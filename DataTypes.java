1c69e20055ea68da11cf4167dc5033098c8f54cb
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-08 13:26:59 +0200

    Memory and size of primitive datatypes

---
+++ DataTypes.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class DataTypes {
+	public static void main(String args[]) {
+		  Scanner sc = new Scanner(System.in);
+
+//          System.out.println("minimum value: "+ Integer.MIN_VALUE);
+//          System.out.println("maxmum value: "+ Integer.MAX_VALUE);
+//          System.out.println("minimum value: "+ Integer.BYTES);
+//          System.out.println("minimum value: "+ Short.MIN_VALUE);
+//          System.out.println("minimum value: "+ Short.BYTES +" bytes");
+//          System.out.println("minimum value: "+ Character.BYTES +" bytes");
+//          char alph = 65; "A" 97 "a"
+//		  0 = 48;
+          //WE USE SINGLE QUOTE FOR CHAR
+//          alph+=8;
+		  
+//		  for(char i = 0x370; i <= 0x3FF; i++) {
+//              
+//	          System.out.print(i);
+//		  } // FOR HEXADECIMAL CHARs
+
+//          System.out.println(alph);
+          
+		  
+//		  char alph = 48; //0
+
+//		  char alph = 57; //9
+
+//		  char alph = 97; //a
+
+		  char alph = 122;
+		  System.out.print(alph); //0
+		  
+		}
+}
