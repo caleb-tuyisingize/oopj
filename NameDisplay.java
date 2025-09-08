91237a9ee0032f39482a2db6f8931597c7e73be5
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-08 10:50:37 +0200

    The java first project commit

---
+++ NameDisplay.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class NameDisplay {
+		public static void main(String args[]) {
+		  Scanner sc = new Scanner(System.in);
+		  
+//		  INPUT
+
+		  System.out.print("Enter firstname : ");
+		  
+		  String firstname = sc.next();
+
+		  System.out.print("Enter lastname : ");
+		  
+		  String lastname = sc.next();
+		  
+//		  PROCESS
+
+//		  OUTPUT
+		  
+		  System.out.println("\n---- Result -----");
+		  System.out.println("Good morning "+firstname+" "+lastname +" You are welcome to the project" );
+		 
+		  
+		}
+	}
+
