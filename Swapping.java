97eb508f26c6d4c6926811852e7d3a9d5468f4d5
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-09 15:10:14 +0200

    Converting datatypes in java

---
+++ Swapping.java
@@ -0,0 +1,26 @@
+class Swapping{
+    public static void main(String arg[]){
+        // TYPE CASTING
+        float a = 10;
+        int b = (int) a;
+        System.out.println(b);//10  BECAUSE THERE IS NO DECIMAL VALUE ON THAT FLOAR VARIABLE
+
+        // CONVERTING
+        // from integer to binary string
+
+        int number = 41;
+        String binary = Integer.toBinaryString(number);
+        System.out.println("In integers: "+number);
+        System.out.println("In binaries: "+binary);
+
+        // from float to bits
+
+        float var = 10.5;
+        int inBits = Float.floatToInBits(var);
+
+        
+        System.out.println("In Float: "+var);
+        System.out.println("In Bits: "+inBits);
+
+    }
+}
