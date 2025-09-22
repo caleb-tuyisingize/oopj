1406ddc678b5d64aa308a57312804d236b6bec7b
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-22 09:49:53 +0200

    Saying the day's names

--- GradeIn.java
+++ GradeIn.java
@@ -55,8 +55,8 @@ public class GradeIn{
 //        System.out.println(z);
 //        System.out.printf("Floated divide: %.2f \n",((float)a/(float)b));  // false (10 is not equal to 20)
 //        System.out.println(a != b);  // true  (10 is not equal to 20)
-        System.out.println("Enter the marks here: ");
-        int marks = sc.nextInt();
+        // System.out.println("Enter the marks here: ");
+        // int marks = sc.nextInt();
         // if(marks >= 80 && marks <=100) {
         // 	System.out.println("Grade A");
         // }
@@ -86,25 +86,55 @@ public class GradeIn{
         //   }
 
         // BY USING SWITCH CASE
-        switch(marks){
-            case 80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100:
-                System.out.println("Grade A");
+        // switch(marks){
+        //     case 80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100:
+        //         System.out.println("Grade A");
+        //         break;
+        //     case 60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79:
+        //         System.out.println("Grade B");
+        //         break;
+        //     case 40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58:
+        //         System.out.println("Grade C");
+        //         break;
+        //     case 0,1,2,3,4,5,6,7,8,9,
+        //          10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19,
+        //          20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29,
+        //          30 ,31 ,32 ,33 ,34 ,35 ,36 ,37 ,38 ,39:
+        //         System.out.println("Grade F");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid Marks");
+        // } 
+
+
+        // USING SWITCH CASE BY SAYING THE DAY'S NAMES
+        System.out.println("Enter the day number (1-7): ");
+        int day = sc.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("Monday");
                 break;
-            case 60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79:
-                System.out.println("Grade B");
+            case 2:
+                System.out.println("Tuesday");
                 break;
-            case 40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58:
-                System.out.println("Grade C");
+            case 3:
+                System.out.println("Wednesday");
                 break;
-            case 0,1,2,3,4,5,6,7,8,9,
-                 10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19,
-                 20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29,
-                 30 ,31 ,32 ,33 ,34 ,35 ,36 ,37 ,38 ,39:
-                System.out.println("Grade F");
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
                 break;
             default:
-                System.out.println("Invalid Marks");
-        } 
+                System.out.println("Invalid day number! Please enter a number between 1 and 7.");
+        }
      
    }
 }
