ab3bc4f74cf630180b2501a48a2f5d984d2f2186
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-22 09:47:05 +0200

    .

--- GradeIn.java
+++ GradeIn.java
@@ -57,12 +57,54 @@ public class GradeIn{
 //        System.out.println(a != b);  // true  (10 is not equal to 20)
         System.out.println("Enter the marks here: ");
         int marks = sc.nextInt();
-        if(marks >= 80 && marks <=100) {
-        	System.out.println("Grade A");
-        }
+        // if(marks >= 80 && marks <=100) {
+        // 	System.out.println("Grade A");
+        // }
+        // else if(marks >= 60 && marks < 80) {
+        // 	System.out.println("Grade B");
+        // }
+        // else if(marks >= 40 && marks < 60) {
+        // 	System.out.println("Grade C");
+        // }
+        // else if(marks >= 0 && marks < 40) {
+        // 	System.out.println("Grade F");
+        // }
+        // else {
+        // 	System.out.println("Invalid Marks");
+        // }
 
-        
-        
+        // CHECKING ODD AND EVEN NUMBERS
+
+        //   if(marks > 0){
+        //     if(marks % 2 == 0){
+        //         System.out.println("Even Number");
+        //     } else {
+        //         System.out.println("Odd Number");
+        //     }   
+        //   }else{
+        //     System.out.println("Invalid Number");
+        //   }
+
+        // BY USING SWITCH CASE
+        switch(marks){
+            case 80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100:
+                System.out.println("Grade A");
+                break;
+            case 60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79:
+                System.out.println("Grade B");
+                break;
+            case 40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58:
+                System.out.println("Grade C");
+                break;
+            case 0,1,2,3,4,5,6,7,8,9,
+                 10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19,
+                 20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29,
+                 30 ,31 ,32 ,33 ,34 ,35 ,36 ,37 ,38 ,39:
+                System.out.println("Grade F");
+                break;
+            default:
+                System.out.println("Invalid Marks");
+        } 
      
    }
 }
