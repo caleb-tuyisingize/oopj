cb4946b058a22b2d117e48a7baa7639d245c26f9
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-30 16:24:22 +0200

    .

---
+++ LoopingFourWeeksOfMonth.java
@@ -0,0 +1,18 @@
+
+public class LoopingFourWeeksOfMonth {
+    public static void main(String[] args) {
+        int counter = 0;
+        for(int year = 1; year <= 12; year++){
+        for (int week = 1; week <= 4; week++) {
+            System.out.print("Week " + week + ": ");
+            for (int day = 1; day <= 7; day++) {
+                counter += 1;
+                System.out.printf("%2d| ", counter);
+            }
+            System.out.println(""); // Blank line between weeks
+        }
+        System.out.println("===================================");
+        System.out.println();
+        }
+    }
+}
