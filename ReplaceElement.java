db6fc55c7ecad29bafb42cb0e74b53e45603227a
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-13 09:49:50 +0200

    ,

---
+++ ReplaceElement.java
@@ -0,0 +1,14 @@
+
+public class ReplaceElement{
+    public static void main(String[] args){
+    int number[];
+    number = new int[10];
+    number[0] = 34;
+    number[1] = 7;
+    number[2] = 20;
+    number[3] = 12;
+    number[4] = 10;
+    
+    
+    }
+}
