905754e4d38eaebc061cfd458aaae39233c426be
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-14 12:36:31 +0200

    .

--- Regex.java
+++ Regex.java
@@ -22,7 +22,23 @@ public class Regex{
 
         String domain = "programmer@gmail.com";
         int atSymbolIndex = domain.indexOf("@");
-        String domain_name = domain.substring(atSymbolIndex + 1, domain.length());
+        int comSymbolIndex = domain.indexOf(".");
+        String domain_name = domain.substring(0,atSymbolIndex);
         System.out.println(domain_name);
+
+        String date = "20/12/2025";
+        // For not exceeding a specified year
+        String pattern = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(202[3-4]|2030)$";
+        boolean matches = date.matches(pattern);
+        System.out.println("Pattern found: " + matches);
+
+        String binaryCheck = "21010101";
+        String binaryPattern = "[0-1]+$";
+        if(binaryCheck.matches(binaryPattern)){
+            System.out.println(binaryCheck + " is a valid binary number.");
+        } else {
+            System.out.println(binaryCheck + " is not a valid binary number.");
+        }
+        // System.out.print(date);
     }   
 }
